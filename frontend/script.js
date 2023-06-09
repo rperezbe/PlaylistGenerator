8248de6828fd2d50379e7befedbac9c594caca5e
Roger Pérez <rperezb@campus.eug.es>
2023-06-09 18:29:10 +0200

    Utilizamos axios para peticiones al servidor

--- frontend/script.js
+++ frontend/script.js
@@ -10,4 +10,32 @@ function showRegister() {
 
   function login() {}
   
-  function register() {}
+  function register() {
+    const username = document.getElementById('new-username').value;
+    const password = document.getElementById('new-password').value;
+    const email = ''; 
+    const age = 0; 
+    const musicPreferences = [];
+  
+    // Crear el objeto con los datos del usuario
+    const newUser = {
+      username,
+      password,
+      email,
+      age,
+      musicPreferences
+    };
+  
+    // Realizar la solicitud POST al backend para crear un usuario
+    axios.post('/users', newUser)
+      .then(response => {
+        // Manejar la respuesta del backend
+        console.log(response.data);
+        // SI SE REGISTRA BIEN VAMOS AL INDEX NO??
+      })
+      .catch(error => {
+        // Manejar el error en caso de que ocurra
+        console.error(error);
+      });
+  }
+  
