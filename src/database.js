b7e59e39315968b7cc63a47311e386f24fc36e67
Roger Pérez <rperezb@campus.eug.es>
2023-06-09 17:52:05 +0200

    implementamos variables de entorno

--- src/database.js
+++ src/database.js
@@ -1,6 +1,13 @@
 //aquí tendremos la conexion a la base de datos con mongoose
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mongo/playlistdb').
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbName = process.env.DB_NAME;
+
+const connectionString = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+
+mongoose.connect(connectionString).
     then(db => console.log('DB is connected to', db.connection.host))
     .catch(err => console.error(err));
