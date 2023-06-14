4b1338288e2418502912917b2699be01191b58aa
charliecaufield <charliecaufield@gmail.com>
2023-06-14 10:04:15 +0200

    Refresca token y stats provisionales

--- src/public/home_script.js
+++ src/public/home_script.js
@@ -1,33 +1,19 @@
-const clientId = 'd7cdacc29b064ae5984e5ca69d5f42e9'; // ID de cliente de tu aplicación
-const clientSecret = '567a2ba4f0c843faad2fdb94b7d375e9'; // Clave secreta de tu aplicación
-const redirectUri = 'http://127.0.0.1:5500/PlaylistGenerator/src/public/callback.html'; // URL de redireccionamiento de tu aplicación
-const accessTokenKey = 'spotify_access_token'; // Clave para almacenar el token de acceso en el almacenamiento local
-const username = 'Robin'; // Nombre de usuario de Spotify
+const clientId = 'd7cdacc29b064ae5984e5ca69d5f42e9'; // ID de cliente
+const clientSecret = '567a2ba4f0c843faad2fdb94b7d375e9'; // clave de cliente
+const redirectUri = 'http://127.0.0.1:5500/PlaylistGenerator/src/public/callback.html'; // URL de redireccionamiento de nuestra aplicacion
+const accessTokenKey = 'spotify_access_token'; // constante para almacenar el access token en el almacenamiento local
+const refreshTokenKey = 'spotify_refresh_token'; // constante para almacenar el refresh token en el almacenamiento local
+const username = 'Robin'; // nombre usuario (lo obtendremos por sesion)
+const genreSeeds = ['rock', 'pop', 'hip-hop', 'metal', 'jazz', 'reggaeton']; //prefencias musicales del usuario (lo obtendremos por sesion)
 
-// Función para generar el URL de autorización de Spotify
-function generateAuthorizationUrl() {
-  const scopes = ['playlist-modify-private']; // Lista de los permisos que necesita tu aplicación
-  const state = generateRandomString(16); // Genera un estado aleatorio para prevenir ataques CSRF
-
-  const url = 'https://accounts.spotify.com/authorize?' +
-    'response_type=code' +
-    '&client_id=' + encodeURIComponent(clientId) +
-    '&scope=' + encodeURIComponent(scopes.join(' ')) +
-    '&redirect_uri=' + encodeURIComponent(redirectUri) +
-    '&state=' + encodeURIComponent(state);
-
-  return url;
-}
-
-// Función para intercambiar el código de autorización por un token de acceso
-function exchangeAuthorizationCode(code) {
+// Funcion para refrescar el token de acceso cuando expire
+function refreshAccessToken(refreshToken) {
   const tokenUrl = 'https://accounts.spotify.com/api/token';
   const encodedCredentials = btoa(clientId + ':' + clientSecret);
 
   const data = {
-    grant_type: 'authorization_code',
-    code: code,
-    redirect_uri: redirectUri
+    grant_type: 'refresh_token',
+    refresh_token: refreshToken
   };
 
   const headers = {
@@ -44,81 +30,21 @@ function exchangeAuthorizationCode(code) {
       if (response.ok) {
         return response.json();
       } else {
-        throw new Error('Failed to exchange authorization code for access token');
+        throw new Error('Failed to refresh access token');
       }
     })
-    .then(data => data.access_token);
+    .then(data => {
+      const newAccessToken = data.access_token;
+      localStorage.setItem(accessTokenKey, newAccessToken);
+      return newAccessToken;
+    });
 }
 
-// Genera una cadena aleatoria de longitud especificada
-function generateRandomString(length) {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
-// Lógica para manejar el callback de autorización
-function handleAuthorizationCallback() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const code = urlParams.get('code');
-  const state = urlParams.get('state');
-
-  if (code && state) {
-    exchangeAuthorizationCode(code)
-      .then(accessToken => {
-        localStorage.setItem(accessTokenKey, accessToken);
-
-        // Elimina los parámetros de la URL para evitar que se ejecute el callback nuevamente
-        window.history.replaceState({}, document.title, redirectUri);
-
-        // Llama a las funciones necesarias una vez que se obtenga el token de acceso
-        // Para obtener un nuevo token de acceso con mayor grado de autorización, simplemente llama a exchangeAuthorizationCode nuevamente con el nuevo código
-        generatePlaylists();
-      })
-      .catch(error => {
-        console.error('Error al intercambiar el código de autorización:', error);
-      });
-  } else {
-    console.error('El código de autorización o el estado no están presentes en la URL');
-  }
-}
-
-// Función para obtener el token de acceso almacenado en el almacenamiento local
-function getAccessToken() {
-  return localStorage.getItem(accessTokenKey);
-}
-
-// Función para iniciar el proceso de autorización
-function authorize() {
-  const accessToken = getAccessToken();
-
-  if (accessToken) {
-    // Si ya se ha obtenido el token de acceso, se puede llamar directamente a la función generatePlaylists()
-    generatePlaylists();
-  } else {
-    const authorizationUrl = generateAuthorizationUrl();
-    window.location.href = authorizationUrl;
-  }
-}
-
-// Llama a la función handleAuthorizationCallback() si la página actual es la de callback.html
-if (window.location.pathname.includes('callback.html')) {
-  handleAuthorizationCallback();
-} else {
-  // Aquí puedes agregar un botón o un evento que inicie el proceso de autorización
-  authorize();
-}
-
-const genreSeeds = ['rock', 'pop', 'hip-hop'];
-
-function getUserId() {
+// Funcion para obtener id de usuario
+function getUserId(accessToken) {
   const url = 'https://api.spotify.com/v1/me';
   const headers = {
-    'Authorization': 'Bearer ' + getAccessToken()
+    'Authorization': 'Bearer ' + accessToken
   };
 
   return fetch(url, {
@@ -134,46 +60,79 @@ function getUserId() {
     .then(data => data.id);
 }
 
+// Funcion para manejar la expiracion del token
+function handleTokenExpiration() {
+  const accessToken = getAccessToken();
+  console.log('Access token:', accessToken);
+  const refreshToken = getRefreshToken();
+  console.log('Refresh token:', refreshToken);
 
-function generatePlaylists() {
-  getUserId()
-    .then(userId => {
-      createPlaylist(userId)
-        .then(playlistId => {
-          const songPromises = genreSeeds.map(genre => getSongsByGenre(genre));
-          Promise.all(songPromises)
-            .then(songsByGenre => {
-              const tracks = songsByGenre.flatMap(songs => songs.slice(0, 2));
-              addTracksToPlaylist(playlistId, tracks)
-                .then(() => {
-                  console.log('Playlist creada y canciones añadidas');
-                  showPlaylist(playlistId);
-                })
-                .catch(error => {
-                  console.error('Error al añadir canciones a la playlist:', error);
-                });
+  if (accessToken) {
+    getUserId(accessToken)
+      .then(userId => {
+        console.log('User ID:', userId);
+        generatePlaylists(userId);
+      })
+      .catch(error => {
+        console.error('Error getting user ID:', error);
+        if (error.message === 'Failed to get user ID') {
+          refreshAccessToken(refreshToken)
+            .then(newAccessToken => {
+              console.log('Access token refreshed:', newAccessToken);
+              generatePlaylists(userId);
             })
             .catch(error => {
-              console.error('Error al obtener canciones por género:', error);
+              console.error('Error refreshing access token:', error);
+              //añade un texto para indicar que el token ha expirado, que hay que recargarlo
+              const main = document.getElementById('playlists');
+              main.innerHTML = '<p>El token de acceso ha expirado. Pulsa el botón para obtener un nuevo token.</p>';
+            });
+        }
+      });
+  } else {
+    //si no hay token de acceso, se redirige a la pagina de autorizacion de spotify
+    console.error('Access token or refresh token not found');
+    const scopes = 'user-read-private user-read-email playlist-modify-private playlist-modify-public user-top-read';
+    window.location.href = 'https://accounts.spotify.com/authorize?client_id=' + clientId + '&response_type=code&redirect_uri=' + redirectUri + '&scope=' + encodeURIComponent(scopes);
+  }
+}
+
+// Funcion para generar playlists
+function generatePlaylists(userId) {
+  createPlaylist(userId)
+    .then(playlistId => {
+      const songPromises = genreSeeds.map(genre => getSongsByGenre(genre));
+      Promise.all(songPromises)
+        .then(songsByGenre => {
+          const tracks = songsByGenre.flatMap(songs => songs.slice(0, 2));
+          addTracksToPlaylist(playlistId, tracks)
+            .then(() => {
+              console.log('Playlist created and songs added');
+              showPlaylist(playlistId);
+            })
+            .catch(error => {
+              console.error('Error adding songs to the playlist:', error);
             });
         })
         .catch(error => {
-          console.error('Error al crear la playlist:', error);
+          console.error('Error getting songs by genre:', error);
         });
     })
     .catch(error => {
-      console.error('Error al obtener el ID de usuario:', error);
+      console.error('Error creating the playlist:', error);
     });
 }
 
+// crea la playlist en el perfil del usuario
 function createPlaylist(userId) {
   const url = `https://api.spotify.com/v1/users/${userId}/playlists`;
   const headers = {
     'Authorization': 'Bearer ' + getAccessToken(),
     'Content-Type': 'application/json'
   };
-  const playlistName = 'Playlist personalizada de ' + username;
-  const playlistDescription = 'Playlist creada para las preferencias musicales de ' + username;
+
+  const playlistName = 'Playlist personalizada para ' + username;
+  const playlistDescription = 'Playlist creada segun las preferencias musicales de ' + username;
 
   const data = {
     name: playlistName,
@@ -194,11 +153,12 @@ function createPlaylist(userId) {
       }
     })
     .then(data => {
-      console.log('Playlist creada');
+      console.log('Playlist created');
       return data.id;
     });
 }
 
+// Funcion para obtener 2 canciones por genero
 function getSongsByGenre(genre) {
   const url = `https://api.spotify.com/v1/recommendations?limit=2&seed_genres=${genre}`;
   const headers = {
@@ -218,6 +178,7 @@ function getSongsByGenre(genre) {
     .then(data => data.tracks.map(track => track.uri));
 }
 
+// Funcion para agregar canciones a la playlist
 function addTracksToPlaylist(playlistId, tracks) {
   const url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
   const headers = {
@@ -242,6 +203,8 @@ function addTracksToPlaylist(playlistId, tracks) {
       }
     });
 }
+
+// Funcion para mostrar la playlist
 function showPlaylist(playlistId) {
   const playlistsContainer = document.getElementById('playlists');
 
@@ -258,4 +221,97 @@ function showPlaylist(playlistId) {
 
   playlistElement.appendChild(iframeElement);
   playlistsContainer.appendChild(playlistElement);
+
+}
+
+// Recibe el access token de local storage
+function getAccessToken() {
+  return localStorage.getItem(accessTokenKey);
+}
+
+// Recibe el refresh token de local storage
+function getRefreshToken() {
+  return localStorage.getItem(refreshTokenKey);
+}
+
+// Funcion que maneja la autorizacion de callback.html
+function handleAuthorizationCallback() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get('code');
+
+  if (code) {
+    getAccessTokenFromCode(code)
+      .then(accessToken => {
+        console.log('Access token:', accessToken);
+        const refreshToken = urlParams.get('refresh_token');
+        console.log('Refresh token:', refreshToken);
+
+        localStorage.setItem(accessTokenKey, accessToken);
+        localStorage.setItem(refreshTokenKey, refreshToken);
+        window.location.href = redirectUri.replace('callback.html', 'home.html');
+      })
+      .catch(error => {
+        console.error('Error getting access token from code:', error);
+      });
+  } else {
+    console.error('Authorization code not found');
+  }
+}
+
+// Function to get the access token from the authorization code
+function getAccessTokenFromCode(code) {
+  const tokenUrl = 'https://accounts.spotify.com/api/token';
+  const encodedCredentials = btoa(clientId + ':' + clientSecret);
+
+  const data = {
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: redirectUri
+  };
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic ' + encodedCredentials
+  };
+
+  return fetch(tokenUrl, {
+    method: 'POST',
+    headers: headers,
+    body: new URLSearchParams(data)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to get access token from code');
+      }
+    })
+    .then(data => data.access_token);
+}
+
+// Si se pulsa el boton de refrescar token -> manejar la expiracion del token
+const refreshButton = document.getElementById('refresh_button');
+refreshButton.addEventListener('click', () => {
+  const refreshToken = getRefreshToken();
+  console.log('Refresh token:', refreshToken);
+
+  if (refreshToken) {
+    refreshAccessToken(refreshToken)
+      .then(newAccessToken => {
+        console.log('Access token refreshed:', newAccessToken);
+        handleTokenExpiration();
+      })
+      .catch(error => {
+        console.error('Error refreshing access token:', error);
+      });
+  } else {
+    console.error('Refresh token not found');
+  }
+});
+
+// verifica si estamos en el callback si no lo estamos verifica si el token expiro
+if (window.location.href.includes('callback.html')) {
+  handleAuthorizationCallback();
+} else {
+  handleTokenExpiration();
 }
